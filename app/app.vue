3dbfc6b8b6459fdc043a08ab2a4ce6f1008ea9a2
Alexandr Basiuk <basok.sanya@gmail.com>
2025-10-20 19:48:24 +0300

    add navigation buttons

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,9 @@
 <template>
   <NuxtPage />
+  <div class="fixed-controls">
+    <nuxt-link class="link-primary link" to="/#resume">Resume</nuxt-link>
+    <nuxt-link class="link-secondary link" to="/#about-me">Contacts</nuxt-link>
+  </div>
 </template>
 
 <style>
@@ -11,4 +15,42 @@
 @import "~/assets/css/brand-identity-section.css";
 @import "~/assets/css/printing-section.css";
 @import "~/assets/css/illustration-section.css";
+
+.fixed-controls {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 10px;
+  z-index: 1000;
+
+  display: flex;
+  align-items: center;
+}
+
+.fixed-controls .link:first-child {
+  border-radius: 10px 0 0 10px;
+}
+.fixed-controls .link:last-child {
+  border-radius: 0 10px 10px 0;
+}
+
+.link-primary {
+  background: var(--primary-color);
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 16px;
+}
+
+.link-secondary {
+  background: var(--primary-dark-color);
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 16px;
+}
 </style>
